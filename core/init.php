ab33a5dddd10c9f47456e7d6fa628ea2922695c6
25564 <cool37132@hotmail.com>
2014-09-02 18:53:08 +1000

    User Session Query fix

--- core/init.php
+++ core/init.php
@@ -29,12 +29,11 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/functions/sanatize.php";
 if(Cookie::exists(Config::get("remember/cookie_name")) && !Session::exists(Config::get("session/session_name"))){ //Checks users is not logged in and cookie exists
 	
 	$hash = Cookie::get(Config::get("remember/cookie_name")); //Get local cookie
-	$hashCheck = DB::getInstance()->get('user_sessions', array('hash', '=', $hash)); //Get hash from DB for comparision
+	$hashCheck = DB::getInstance()->table('user_sessions')->where('hash', $hash)->get(); //Get hash from DB for comparision
 	
-	if($hashCheck->count() != 0){
-		
-		if($hash == $hashCheck->first()->hash){ //Check they match
-			Session::put(Config::get("session/session_name"), $hashCheck->first()->user_id); //Create Session
+	if(count($hashCheck) != 0){
+		if($hash == $hashCheck[0]->hash){ //Check they match
+			Session::put(Config::get("session/session_name"), $hashCheck[0]->user_id); //Create Session
 		}
 		else {
 			Cookie::delete("remember/cookie_name"); //If they don't match may aswell delete it so this check does not keep getting ran
